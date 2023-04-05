faf7756bff58954fa20de5a8ce9e9a773af595e6
Luis Parada <luis94sh@gmail.com>
2023-04-05 13:59:47 -0400

    dd

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
+const allowedChannel = -1111111111 // replace with your desired channel name or ID
+const forexChannel = -1111111111// replace with your desired channel name or ID
+const stockChannel = -11111111111 // replace with your desired channel name or ID
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 let url = 'https://fapi.binance.com'
